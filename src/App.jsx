2215ac0cfa4cedb2b3f5939518b6257ae77b1a2f
Danial-Hosseintabar <danial.ht10@gmail.com>
2024-08-21 01:07:20 +0330

    particles for background

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,23 @@ import HelloCard from './HelloCard.jsx'
 import Skills from './Skills.jsx'
 import Experiences from './Experiences.jsx'
 import Footer from './Footer.jsx'
+import Background from './Background.jsx'
 import './App.css'
 
 function App() {
 
     return (
         <>
+            <Background />
             <Header />
-            <HelloCard />
-            <HelloCard />
-            <HelloCard />
             <HelloCard />
             <Skills />
             <Experiences />
             <Footer />
         </>
     );
-
 }
+
+
 
 export default App
