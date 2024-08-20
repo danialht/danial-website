e411577518d1ab67889966436a99ba4a2246a548
Danial-Hosseintabar <danial.ht10@gmail.com>
2024-08-20 22:47:08 +0330

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Header from './Header.jsx'
+import About from './About.jsx'
+import Skills from './Skills.jsx'
+import Experiences from './Experiences.jsx'
+import Footer from './Footer.jsx'
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+            <Header />
+            <About />
+            <Skills />
+            <Experiences />
+            <Footer />
+        </>
+    );
+
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li>About</li>
+                    <li>Skills</li>
+                    <li>Experiences</li>
+                </ul>
+            </nav>
+            <hr />
+        </>
+    );
+}
+
+export default Header
